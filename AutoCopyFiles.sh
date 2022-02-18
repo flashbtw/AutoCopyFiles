b64e9dc7887cb55e34e64eb7278a40a3d4e24d07
flashbtw <74975609+flashbtw@users.noreply.github.com>
2022-02-18 13:20:49 +0100

    Update and Rename AutoCopyFile.sh

--- AutoCopyFiles.sh
+++ AutoCopyFiles.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 targetDirectory="/path/to/targetdirectory" # Without a slash at the end!
+watchedPath = "/path/to/watch/"
+fileOwner = "root" # Sets which user is going to own the file in the target directory
 
 while :
 do
 
   # as this is recursive it is also going to trigger when you copy something in a subdirectory of that path
-  pathToFile=`inotifywait -r -q -e close_write --format '%w%f' /path/to/watch` 
+  pathToFile=`inotifywait -r -q -e close_write --format '%w%f' $watchedPath` 
   filename=`echo $pathToFile | rev | cut -f 1 -d / | rev`
   
   echo "copying $pathToFile to $targetDirectory/$filename and removing it from source afterwards"  
@@ -14,6 +16,6 @@ do
   rm $pathToFile
   
   echo "Setting up rights for $filename"
-  chown mc:mc $targetDirectory/$filename
+  chown $fileOwner:$fileOwner $targetDirectory/$filename
 
 done
